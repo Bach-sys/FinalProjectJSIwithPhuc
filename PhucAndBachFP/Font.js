7d1fc7004cb192bce3bda4928491e64cad0d62e6
Bach-sys <bachloppho1202@gmail.com>
2023-06-20 08:40:05 +0700

    delete the back.js because it have coded wrong way

---
+++ PhucAndBachFP/Font.js
@@ -0,0 +1,20 @@
+import "./style.css";
+import javascriptLogo from "./javascript.svg";
+import viteLogo from "/vite.svg";
+import { setupCounter } from "./counter.js";
+
+document.querySelector("#app").innerHTML = `
+<a href="#" class="link-img"><img src="logo.png" alt="" class="img"></a>
+<div class="nav-child">
+    <a class="btn" href="#">Home</a>
+    <a class="btn" href="#">About us</a>
+    <a class="btn" href="#">Contact</a>
+    <a class="btn" href="#">Book tour</a>
+</div>
+<div class="register">
+    <a class="btn-log" href="#">Log in</a>
+    <a class="btn-si" href="#">Sign up</a>
+</div>
+`;
+
+console.log("hello Apple");
