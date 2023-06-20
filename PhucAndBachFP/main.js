6ee4b4a8694cd976f72db864321995c18bec6dac
hello <nhoangphuc960@gmail.com>
2023-06-20 18:50:57 +0700

    Fixbug

--- PhucAndBachFP/main.js
+++ PhucAndBachFP/main.js
@@ -3,10 +3,24 @@ import javascriptLogo from "./javascript.svg";
 import viteLogo from "/vite.svg";
 import { setupCounter } from "./counter.js";
 
-let Signup = document.querySelector("#signup")
-let Login = document.querySelector("#login")
+let Register = document.querySelector("#register")
+let btnlog = document.querySelector("#Login")
+let btnsign = document.querySelector("#Signup")
+document.querySelector("#navbar").innerHTML = `
+<a href="#" class="link-logo"><img src="logo.png" alt="" class="logo"></a>
+<div class="nav-child">
+    <a class="btn" href="#">Home</a>
+    <a class="btn" href="#">About us</a>
+    <a class="btn" href="#">Contact</a>
+    <a class="btn" href="#">Book tour</a>
+</div>
+<div class="register">
+    <button class="btn-re" id="Login" >Log in</button>
+    <button class="btn-re" id="Signup">Sign up</button>
+</div>
+`;
 let signupRender = () => {
-    Signup.innerHTML=`
+    Register.innerHTML=`
     <img src="https://i.pinimg.com/564x/60/04/2b/60042bd77056155bd25412fa223c8eeb.jpg" alt="" class="login-img">
     <div class="form-container">
       <div class="logo-container">
@@ -31,15 +45,14 @@ let signupRender = () => {
 
         <button type="submit" class="form-submit-btn">Sign up</button>
       </div>
-      <p class="signup-link">
+      <p class="ques-link">
         Have an account?
-        <a class="signup-link link" href="#"> Log In now</a>
+        <button class="register-link link"> Log In now</button>
       </p>
     </div>`
 }
-signupRender()
 let loginRender = () => {
-  Login.innerHTML=`
+  Register.innerHTML=`
   <img src="https://i.pinimg.com/564x/60/04/2b/60042bd77056155bd25412fa223c8eeb.jpg" alt="" class="login-img">
   <div class="form-container">
     <div class="logo-container">
@@ -59,24 +72,15 @@ let loginRender = () => {
 
       <button type="submit" class="form-submit-btn">Log In</button>
     </div>
-    <p class="signup-link">
+    <p class="ques-link">
       Have an account?
-      <a class="signup-link link" href="#"> Sign up now</a>
+      <button class="register-link link" href="#"> Sign up now</button>
     </p>
   </div>`
 }
-loginRender()
-document.querySelector("#app").innerHTML = `
-<a href="#" class="link-logo"><img src="logo.png" alt="" class="logo"></a>
-<div class="nav-child">
-    <a class="btn" href="#">Home</a>
-    <a class="btn" href="#">About us</a>
-    <a class="btn" href="#">Contact</a>
-    <a class="btn" href="#">Book tour</a>
-</div>
-<div class="register">
-    <a class="btn-re" href="#">Log in</a>
-    <a class="btn-re" href="#">Sign up</a>
-</div>
-`;
-
+btnlog.addEventListener("click", () => {
+  loginRender()
+})
+btnsign.addEventListener("click", () => {
+  signupRender()
+})
