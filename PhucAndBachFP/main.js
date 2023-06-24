d884ebe12f461c2228608b25a7e8cc89d91337a5
hello <nhoangphuc960@gmail.com>
2023-06-24 19:10:17 +0700

    Home-page

--- PhucAndBachFP/main.js
+++ PhucAndBachFP/main.js
@@ -1,22 +1,54 @@
 import "./style.css";
 import javascriptLogo from "./javascript.svg";
-import { setupCounter } from "./counter.js";
+
 
 let Register = document.querySelector("#register");
-let navMain = document.querySelector("#navbar-main")
-let navMainRender = () =>{
-  navMain.innerHTML = `
-`;
+let nav = document.querySelector("#nav")
+let home = document.querySelector("#home-page")
+let navRender = () => {
+  nav.innerHTML=`
+  <div id="nav-main-img">
+  <div class="nav-main">
+    <a href="#" class="nav-logo-link"><img class="nav-logo" src="logo.PNG" alt=""></a>
+    <div class="nav-main-child">
+      <button class="btn-nav" id ="home" >Home</button>
+      <button class="btn-nav">About us</button>
+      <button class="btn-nav">Contact</button>
+      <button class="btn-nav">Book tour</button>
+      <button class="btn-nav"id="btn-nav-log">Sign In</button>
+      <button class="btn-nav"id="btn-nav-sign">Sign Up</button>
+    </div>
+  </div>
+  <p>TREKKING</p>
+</div>
+  `
 }
-//!thay đổi ở đây
+navRender()
 let btnlog = document.querySelector("#btn-nav-log");
 let btnsign = document.querySelector("#btn-nav-sign");
+let btnhome = document.querySelector("#home")
+let homeRender = () =>{
+  home.innerHTML=`
+  <div class="introduce">
+  <div class="introduce-child">
+    <div class="introduce-des">
+      <img class="img-introduce" src="logo.png" alt="">
+      <p class="des">Sâu thẳm từ bên trong mỗi chúng ta đều có “những tiếng gọi” từ thiên nhiên hoang dã. Chúng đánh thức bạn đến với sự khát khao được tự do, khám phá về thế giới xung quanh, về thiên nhiên và cội nguồn. Tinh thần hoang dại đó sẽ khiến bạn cảm nhận cuộc sống này sống động hơn, mãnh liệt hơn và thúc đẩy bạn tận hưởng từng cảm xúc ngay khoảnh khắc hiện tại. The Happy Trip là nơi hội tụ những con người yêu núi rừng với đội ngũ giàu kinh nghiệm cùng các chuyên gia về kỹ năng, kiến thức trong lĩnh vực du lịch mạo hiểm. Chúng tôi dành nhiều ngày liền sống trong rừng để hiểu địa hình, cảm nhận văn hóa địa phương và thiết kế những cuộc phiêu lưu độc nhất. Chúng tôi đến để “đứa trẻ” bên trong bạn được tắm gội với núi rừng, hít thở khí trời và trưởng thành với những trải nghiệm mạo hiểm từ thiên nhiên một cách an toàn, chuyên nghiệp và bền vững. Bạn sẽ đi, chinh phục, khám phá, tận hưởng, tự do và “được sống” nhiều hơn. Với sứ mệnh đó, The Happy Trip tin rằng tinh thần hoang dại của bạn cùng với kinh nghiệm và kiến thức chuyên môn của chúng tôi sẽ tạo nên những trải nghiệm độc nhất dành riêng cho bạn. 
+        THE BEST TRIP THE BEST PERSON
+        </p>
+    </div>
+    <img class="des-img" src="img-product.jpg" alt="">
+  </div>
+</div>
+  `
+}
+homeRender();
 let signupRender = () => {
   Register.innerHTML = `
     <img src="https://i.pinimg.com/564x/60/04/2b/60042bd77056155bd25412fa223c8eeb.jpg" alt="" class="login-img">
     <div class="form-container">
       <div class="logo-container">
-        Sign up
+        Sign Up
       </div>
       <div class="line"></div>
       <div class="form">
@@ -48,7 +80,7 @@ let loginRender = () => {
   <img src="https://i.pinimg.com/564x/60/04/2b/60042bd77056155bd25412fa223c8eeb.jpg" alt="" class="login-img">
   <div class="form-container">
     <div class="logo-container">
-      Log In
+      Sign In
     </div>
     <div class="line"></div>
     <div class="form">
@@ -72,11 +104,11 @@ let loginRender = () => {
 };
   btnlog.addEventListener("click", () => {
     loginRender();
-
   });
   btnsign.addEventListener("click", () => {
     signupRender();
-
   });
-
-
+  btnhome.addEventListener("click", () => {
+    navRender()
+    homeRender()
+  });
---
+++ PhucAndBachFP/script.js
@@ -0,0 +1,59 @@
+//Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDZNxTHPNkJM1tX2n0fUVYzKMh1VYnQxL8",
+  authDomain: "finalproject-4227e.firebaseapp.com",
+  projectId: "finalproject-4227e",
+  storageBucket: "finalproject-4227e.appspot.com",
+  messagingSenderId: "928682874879",
+  appId: "1:928682874879:web:a5526cfe1a50acf96139ca",
+  measurementId: "G-8XR793HVFL",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+//khởi tạo biến xác thực firebase gồm phần giả lập và hệ thống đăng nhập
+const auth = getAuth(app);
+console.log(app);
+
+let email;
+let password;
+let btnComfirm = document.querySelector("#btn_comfirm");
+
+window.addEventListener("DOMContentLoaded", (event) => {
+  if (btnComfirm) {
+    btnComfirm.addEventListener("click", () => {
+      email = document.querySelector("#email");
+      password = document.querySelector("#password");
+      var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+      if (email.value.length < 7) {
+      }
+      createUserWithEmailAndPassword(auth, email.value, password.value)
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          console.log(user.email);
+          // ...
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          console.log(errorMessage);
+          // ..
+        });
+    });
+  }
+});
