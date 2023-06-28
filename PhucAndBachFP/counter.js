fa72344c04ef4b3706997530b7c3817e223b9a98
Bach-sys <bachloppho1202@gmail.com>
2023-06-28 21:46:16 +0700

    Authentication

--- PhucAndBachFP/counter.js
+++ PhucAndBachFP/counter.js
@@ -1,10 +1,10 @@
 let regex = new RegExp("[a-z0-9]+@[a-z]+.[a-z]{2,3}");
 
 let testEmails = [
-  "notanemail.com",
-  "workingexample@email.com",
-  "another_working@somethingelse.org",
-  "notworking@1.com",
+  "notanemail.com",//false
+  "workingexample@email.com",//true
+  "another_working@somethingelse.org",//rue
+  "notworking@1.com",//false
 ];
 
 testEmails.forEach((address) => {
